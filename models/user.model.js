0858e2632c9e995ac5355f830acc9e53e1cb978a
Animesh Kalita <104593018+Ak-nut-47@users.noreply.github.com>
2023-08-24 17:26:57 +0530

    Changes

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    username: String,
+    avatar: String,
 })
 
 const UserModel = mongoose.model("user", userSchema)
